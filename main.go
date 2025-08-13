cba45a7eab238bbe92379fd9ce6a3ae35c7b9c41
Amujo Simon <amujosimon01@gmail.com>
2025-08-13 21:54:44 +0100

    finally finished the TODO list

--- main.go
+++ main.go
@@ -6,36 +6,40 @@ func main() {
 	var task  string
 	var choice int
 
-	{
+	for {
 		
 		fmt.Println("MY TODO LIST")
 		fmt.Println("1. Create Task")
 		fmt.Println("2. View Tasks")
 		fmt.Println("3. Exit")
-		
-	}
 
-	switch choice{
-	case 1: 
-		fmt.Println("Input task:")
-		fmt.Scan(&task)
-		tasks = append(tasks, task)
-		fmt.Println("Your task has been added")
-	case 2:
-		fmt.Println("Your Tasks")
-		if len(tasks) == 0 {
-			fmt.Println("No current tasks")
-		} else {
-			for i, t := range tasks {
-				fmt.Println(i+1, t)
+		fmt.Print("pick your choice")3
+		fmt.Scan(&choice)
+		
+	
+
+		switch choice{
+		case 1: 
+			fmt.Println("Input task:")
+			fmt.Scan(&task)
+			tasks = append(tasks, task)
+			fmt.Println("Your task has been added")
+		case 2:
+			fmt.Println("Your Tasks")
+			if len(tasks) == 0 {
+				fmt.Println("No current tasks")
+			} else {
+				for i, t := range tasks {
+					fmt.Println(i+1, t)
+				}
 			}
-		}
-	case 3:
-		fmt.Println("See you later")
-		return
-	default:
-		fmt.Println("Try again")
+		case 3:
+			fmt.Println("See you later")
+			return
+		default:
+			fmt.Println("Try again")
 
 		
+		}
 	}
 }
